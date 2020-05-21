1a8459eb6b74de2405deef671b02d45717ae00de
U-WYBO-WORKPC\wybol <wybosys>
2020-05-21 19:58:15 +0800

    优化ut

--- test/test7.lua
+++ test/test7.lua
@@ -5,7 +5,14 @@ function test7_a()
     t:delete()
 end
 
+function test7_b()
+    local t = test.Test:new(1, 2)
+    assert(t:proc() == "test7", "test7 proc is test7")
+    -- t = nil 没有用，主要靠 gc
+end
+
 function test7()
     test7_a()
+    test7_b()
     collectgarbage("collect")
 end
