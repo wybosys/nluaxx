767a5e6f958dbfe063d9954a13fa400921b40f4e
wybosys <wybosys@gmail.com>
2020-06-08 11:04:26 +0800

    prj

--- src/nlua++.hpp
+++ src/nlua++.hpp
@@ -21,7 +21,8 @@ class Module;
 
 typedef shared_ptr<Variant> return_type;
 typedef shared_ptr<Variant> value_type;
-typedef initializer_list<Variant> args_type;
+typedef Variant arg_type;
+typedef initializer_list<arg_type> args_type;
 typedef shared_ptr<Object> self_type;
 
 class Any {
