c71a63e493c7c5fd3e3d9717f42ec2704e59b464
wybosys <wybosys@gmail.com>
2020-06-29 10:08:41 +0800

    opt

--- nlua++/variant.cpp
+++ nlua++/variant.cpp
@@ -10,28 +10,29 @@ NLUA_BEGIN
 
 USE_CROSS
 
-Variant::VT FromCom(com::Variant<>::VT vt) {
+Variant::VT FromCom(Variant::com_variant_type::VT vt)
+{
     switch (vt) {
-        case com::Variant<>::VT::INT:
-        case com::Variant<>::VT::UINT:
-        case com::Variant<>::VT::LONG:
-        case com::Variant<>::VT::ULONG:
-        case com::Variant<>::VT::SHORT:
-        case com::Variant<>::VT::USHORT:
-        case com::Variant<>::VT::LONGLONG:
-        case com::Variant<>::VT::ULONGLONG:
-        case com::Variant<>::VT::CHAR:
-        case com::Variant<>::VT::UCHAR:
+        case Variant::com_variant_type::VT::INT:
+        case Variant::com_variant_type::VT::UINT:
+        case Variant::com_variant_type::VT::LONG:
+        case Variant::com_variant_type::VT::ULONG:
+        case Variant::com_variant_type::VT::SHORT:
+        case Variant::com_variant_type::VT::USHORT:
+        case Variant::com_variant_type::VT::LONGLONG:
+        case Variant::com_variant_type::VT::ULONGLONG:
+        case Variant::com_variant_type::VT::CHAR:
+        case Variant::com_variant_type::VT::UCHAR:
             return Variant::VT::INTEGER;
-        case com::Variant<>::VT::FLOAT:
-        case com::Variant<>::VT::DOUBLE:
+        case Variant::com_variant_type::VT::FLOAT:
+        case Variant::com_variant_type::VT::DOUBLE:
             return Variant::VT::NUMBER;
-        case com::Variant<>::VT::OBJECT:
-        case com::Variant<>::VT::POINTER:
+        case Variant::com_variant_type::VT::OBJECT:
+        case Variant::com_variant_type::VT::POINTER:
             return Variant::VT::POINTER;
-        case com::Variant<>::VT::BOOLEAN:
+        case Variant::com_variant_type::VT::BOOLEAN:
             return Variant::VT::BOOLEAN;
-        case com::Variant<>::VT::STRING:
+        case Variant::com_variant_type::VT::STRING:
             return Variant::VT::STRING;
         default:
             break;
@@ -40,77 +41,87 @@ Variant::VT FromCom(com::Variant<>::VT vt) {
 }
 
 Variant::Variant()
-        : vt(VT::NIL) {
+    : vt(VT::NIL)
+{
 }
 
 Variant::Variant(integer v)
-        : vt(VT::INTEGER), _var(v) {
+    : vt(VT::INTEGER), _var(v)
+{
 }
 
 Variant::Variant(number v)
-        : vt(VT::NUMBER), _var(v) {
+    : vt(VT::NUMBER), _var(v)
+{
 }
 
 Variant::Variant(bool v)
-        : vt(VT::BOOLEAN), _var(v) {
+    : vt(VT::BOOLEAN), _var(v)
+{
 }
 
 Variant::Variant(string const &v)
-        : vt(VT::STRING), _var(v) {
+    : vt(VT::STRING), _var(v)
+{
 }
 
 Variant::Variant(char const *v)
-        : vt(VT::STRING), _var(v) {
+    : vt(VT::STRING), _var(v)
+{
 }
 
 Variant::Variant(void *v)
-        : vt(VT::POINTER), _var(v) {
+    : vt(VT::POINTER), _var(v)
+{
 }
 
 Variant::Variant(nullptr_t)
-        : vt(VT::POINTER), _var(nullptr) {
+    : vt(VT::POINTER), _var(nullptr)
+{
 }
 
-Variant::Variant(com::Variant<> const &v)
-        : vt(FromCom(v.vt)), _var(v) {
+Variant::Variant(com_variant_type const &v)
+    : vt(FromCom(v.vt)), _var(v)
+{
 }
 
 Variant::Variant(shared_ptr<Object> const &r)
-        : vt(VT::OBJECT), _obj(r)
+    : vt(VT::OBJECT), _obj(r)
 {
     // 兼容android
-    const_cast<com::Variant<> &>(_var) = *r->toComVariant();
+    const_cast<com_variant_type &>(_var) = *r->toComVariant();
 }
 
-integer Variant::toInteger() const {
+integer Variant::toInteger() const
+{
     switch (_var.vt) {
-        case com::Variant<>::VT::INT:
+        case com_variant_type::VT::INT:
             return _var.toInt();
-        case com::Variant<>::VT::UINT:
+        case com_variant_type::VT::UINT:
             return _var.toUInt();
-        case com::Variant<>::VT::LONG:
+        case com_variant_type::VT::LONG:
             return _var.toLong();
-        case com::Variant<>::VT::ULONG:
+        case com_variant_type::VT::ULONG:
             return _var.toULong();
-        case com::Variant<>::VT::SHORT:
+        case com_variant_type::VT::SHORT:
             return _var.toShort();
-        case com::Variant<>::VT::USHORT:
+        case com_variant_type::VT::USHORT:
             return _var.toUShort();
-        case com::Variant<>::VT::LONGLONG:
+        case com_variant_type::VT::LONGLONG:
             return (integer) _var.toLonglong();
-        case com::Variant<>::VT::ULONGLONG:
+        case com_variant_type::VT::ULONGLONG:
             return (integer) _var.toULonglong();
-        case com::Variant<>::VT::CHAR:
+        case com_variant_type::VT::CHAR:
             return _var.toChar();
-        case com::Variant<>::VT::UCHAR:
+        case com_variant_type::VT::UCHAR:
             return _var.toUChar();
-        case com::Variant<>::VT::BOOLEAN:
+        case com_variant_type::VT::BOOLEAN:
             return _var.toBool();
-        case com::Variant<>::VT::FLOAT:
+        case com_variant_type::VT::FLOAT:
             return (integer) round(_var.toFloat());
-        case com::Variant<>::VT::DOUBLE:
+        case com_variant_type::VT::DOUBLE:
             return (integer) round(_var.toDouble());
-        case com::Variant<>::VT::STRING:
+        case com_variant_type::VT::STRING:
             return toint(_var.toString());
         default:
             break;
@@ -118,15 +129,16 @@ integer Variant::toInteger() const {
     return 0;
 }
 
-number Variant::toNumber() const {
+number Variant::toNumber() const
+{
     switch (_var.vt) {
-        case com::Variant<>::VT::FLOAT:
+        case com_variant_type::VT::FLOAT:
             return _var.toFloat();
-        case com::Variant<>::VT::DOUBLE:
+        case com_variant_type::VT::DOUBLE:
             return _var.toDouble();
-        case com::Variant<>::VT::STRING:
+        case com_variant_type::VT::STRING:
             return todouble(_var.toString());
-        case com::Variant<>::VT::NIL:
+        case com_variant_type::VT::NIL:
             return 0;
         default:
             break;
@@ -134,30 +146,32 @@ number Variant::toNumber() const {
     return toInteger();
 }
 
-bool Variant::toBool() const {
-    if (_var.vt == com::Variant<>::VT::BOOLEAN)
+bool Variant::toBool() const
+{
+    if (_var.vt == com_variant_type::VT::BOOLEAN)
         return _var.toBool();
     return toNumber() != 0;
 }
 
-string Variant::toString() const {
-    if (_var.vt == com::Variant<>::VT::STRING)
+string Variant::toString() const
+{
+    if (_var.vt == com_variant_type::VT::STRING)
         return _var.toString();
 
-    if (_var.vt == com::Variant<>::VT::FLOAT ||
-        _var.vt == com::Variant<>::VT::DOUBLE)
-    {
+    if (_var.vt == com_variant_type::VT::FLOAT ||
+        _var.vt == com_variant_type::VT::DOUBLE) {
         return tostr(toNumber());
     }
 
-    return tostr((int)toInteger());
+    return tostr((int) toInteger());
 }
 
-void *Variant::toPointer() const {
+void *Variant::toPointer() const
+{
     switch (_var.vt) {
-        case com::Variant<>::VT::OBJECT:
+        case com_variant_type::VT::OBJECT:
             return _var.toObject();
-        case com::Variant<>::VT::POINTER:
+        case com_variant_type::VT::POINTER:
             return _var.toPointer();
         default:
             break;
